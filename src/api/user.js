503d8c26d5862a919cceb5fa981ac495cf537f9a
codebyjackie <JackieLinDev@foxmail.com>
2024-05-04 23:21:52 -0500

    完成了用户资料、文章管理、分类管理、登录页面的开发及bug修复

--- src/api/user.js
+++ src/api/user.js
@@ -10,3 +10,11 @@ export const userGetInfoService = () => request.get('/my/userinfo')
 
 export const userUpdateInfoService = ({ id, nickname, email }) =>
   request.put('/my/userinfo', { id, nickname, email })
+
+// 更新头像
+export const userUpdateAvatarService = (avatar) =>
+  request.patch('/my/update/avatar', { avatar })
+
+// 更新密码
+export const userUpdatePasswordService = ({ old_pwd, new_pwd, re_pwd }) =>
+  request.patch('/my/updatepwd', { old_pwd, new_pwd, re_pwd })
--- src/views/user/UserAvatar.vue
+++ src/views/user/UserAvatar.vue
@@ -1,7 +1,85 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+import { Plus, Upload } from '@element-plus/icons-vue'
+import { userUpdateAvatarService } from '@/api/user'
+import { useUserStore } from '@/stores'
+
+const userStore = useUserStore()
+const imageUrl = ref(userStore.user.user_pic)
+const uploadRef = ref(null)
+const onSelectFile = (file) => {
+  const reader = new FileReader()
+  reader.onload = (e) => {
+    imageUrl.value = e.target.result
+  }
+  reader.readAsDataURL(file.raw)
+}
+const onUpdateAvatar = async () => {
+  await userUpdateAvatarService(imageUrl.value)
+  await userStore.getUser()
+  ElMessage.success('更换头像成功')
+}
+</script>
 
 <template>
-  <div>UserAvatar</div>
+  <page-container title="更换头像">
+    <el-upload
+      class="avatar-uploader"
+      :show-file-list="false"
+      :auto-upload="false"
+      ref="uploadRef"
+      :on-change="onSelectFile"
+    >
+      <img v-if="imageUrl" :src="imageUrl" class="avatar" />
+      <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+    </el-upload>
+    <el-button
+      type="primary"
+      :icon="Plus"
+      size="large"
+      @click="uploadRef.$el.querySelector('input').click()"
+      >选择图片</el-button
+    >
+    <el-button
+      type="success"
+      :icon="Upload"
+      size="large"
+      @click="onUpdateAvatar"
+      >上传头像</el-button
+    >
+  </page-container>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.avatar-uploader {
+  :deep() {
+    .avatar {
+      width: 278px;
+      height: 278px;
+      display: block;
+    }
+    .el-upload {
+      border: 1px dashed var(--el-border-color);
+      border-radius: 6px;
+      cursor: pointer;
+      position: relative;
+      overflow: hidden;
+      transition: var(--el-transition-duration-fast);
+    }
+    .el-upload:hover {
+      border-color: var(--el-color-primary);
+    }
+    .el-icon.avatar-uploader-icon {
+      font-size: 28px;
+      color: #8c939d;
+      width: 278px;
+      height: 278px;
+      text-align: center;
+    }
+  }
+}
+.el-button {
+  margin-left: 15px;
+  margin-top: 20px;
+}
+</style>
